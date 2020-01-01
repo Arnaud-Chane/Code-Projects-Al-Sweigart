4ec954fa4a254dfac3ae2ab04f696629876e07d0
Arnaud-Chane <59416180+Arnaud-Chane@users.noreply.github.com>
2020-01-01 21:00:45 +0100

    Update from test to playable GuessGame

--- GuessGame.py
+++ GuessGame.py
@@ -5,7 +5,7 @@ print("Hello ! What's your name ?")
 myName = input("Hi ! I'm ")
 print(f"Alright, then {myName}, let's play a game ! Guess a number between 0 and 10.")
 
-number = 5
+number = random.randint(0, 10)
 guessesTaken = 0
 answer = 0
 
@@ -68,7 +68,7 @@ if answer == 0:
 if guessNumber == number :
     guessesTaken = str(guessesTaken)
     number = str(number)
-    print(f"Hey ! You find the right number in {guessesTaken} attempts. The number was indeed {number}.")
+    print(f"Hey ! You find the right number in {guessesTaken} attempts. The number was indeed the number {number}.")
     number = int(number)
 
 if guessNumber != number :
